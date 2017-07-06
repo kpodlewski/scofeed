7428501250106e148b3f858c9182cbeb82176d04
kpodlewski <kpodlewski91@gmail.com>
2017-07-06 23:22:16 +0200

    layout ver.1

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+module.exports	=	{
+	entry:	"./js/app.jsx",
+	output:	{
+		publicPath: "./",
+		filename: "js/out.js"
+	},
+	devServer: {
+		inline: true,
+		contentBase: "./",
+		port: 3001
+	},
+	watch:	true,
+	module:	{
+		loaders: [
+			{
+				test: /\.jsx$/,		
+				exclude: /node_modules/,
+				loader:	'babel-loader',
+				query: {
+					presets: ['es2015','stage-2','react']
+				}
+			},
+			{
+				test:/\.(png|jpe?g|gif)$/,
+				exclude:/node_modules/,
+				loader: 'url-loader?limit=1024&name=/img/[name].[ext]'
+			},
+			{
+				test   : /\.css$/,
+				loaders: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.scss$/,		
+				loaders: ['style-loader', 'css-loader', 'sass-loader']
+			}
+		]
+	}
+}
